c70cd357e9b5c7cb76c7cb9ee5df24ecfcde5420
Jacob <jacobvoneckermann@tink.com>
2021-11-02 12:57:52 +0100

    Toggle for test server

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # tinksecretsanta
 
-At the moment, this code only work forward a SMTP test server. Next step is to include a secure login to your mail through SSL, that also can handle two-factor authentication.
+At the moment, this code cannot handle 2FA. You can either provide a dummy email, or run forwards a SMTP Test Server
 
 ## Set-Up
 
-Start a server using this command. 
+First, set testServerEnabled to True if you want to run forwards a server. You will not need any login for this. If this is false, you will need to provide credentials to the mail you want to send from.   
+Then, start a server using this command. 
 `python -m smtpd -n -c DebuggingServer localhost:1025`. 
 Next, fill contacts_file.csv with any testdata you want to use. Then run the script using Python 3.
--- debugPlainEmail.py
+++ debugPlainEmail.py
@@ -2,9 +2,13 @@ import smtplib, ssl, csv
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+testServerEnabled = True # change to true if you want to work forwards a SMPT Test-server
+
 sender_email = "secretsanta.tink@gmail.com"
 #receiver_email = "your@gmail.com"
-password = input("Type your password and press enter:")
+
+if(not testServerEnabled):
+    password = input("Type your password and press enter:")
 
 message = MIMEMultipart("alternative")
 message["Subject"] = "Your Secret Santa"
@@ -19,9 +23,7 @@ This should be the plaintext-version"""
 
 text = """\
 Hi,
-How are you?
-Real Python has many great tutorials:
-www.realpython.com"""
+How are you?"""
 html = """\
 <html>
   <body>
@@ -42,18 +44,31 @@ part2 = MIMEText(html, "html")
 message.attach(part1)
 message.attach(part2)
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
+if(testServerEnabled):
     with open("contacts_file.csv") as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip header row
-        for name, email, secret in reader:
+    	reader = csv.reader(file)
+    	next(reader) # skip header row
+    	for name, email, secret in reader:
+    		try:
+    			server = smtplib.SMTP("localhost", 1025)
+    			server.ehlo()		
+    			server.sendmail(sender_email, email, message.as_string().format(name=name, email=email, secret=secret))
+    		except Exception as e:
+    			print(e)
+
+if(not testServerEnabled):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        with open("contacts_file.csv") as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip header row
+            for name, email, secret in reader:
             
-            message["To"] = email
+                message["To"] = email
             
-            server.sendmail(
-                sender_email,
-                email,
-                message.as_string().format(name=name, email=email, secret=secret),
-            )
+                server.sendmail(
+                    sender_email,
+                    email,
+                    message.as_string().format(name=name, email=email, secret=secret),
+                )
